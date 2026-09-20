154fe8d858bbf011aab32f6784c7fc276689b8df
杜宏泰 <2535692722@qq.com>
2026-09-20 16:51:37 +0800

    g++编译标准工程配置

--- day1_copy/src/main.cpp
+++ day1_copy/src/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world" << endl;
+    cout << "hello world. I'm " << 18 << " today" << endl;
     cout << min(100, 200) << endl;
     return 0;
 }
