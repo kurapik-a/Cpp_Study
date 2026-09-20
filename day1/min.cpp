917b20275c18500f3d8aae7df17979cba5b9a7e9
kurapik-a <2535692722@qq.com>
2026-09-20 10:57:03 +0800

    day1 test

---
+++ day1/min.cpp
@@ -0,0 +1,5 @@
+int min(int a, int b)
+{
+
+    return a < b ? a : b;
+}
---
+++ day1/test1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "min.h"
+
+using namespace std;
+
+int main()
+{
+
+    cout << min(100,200) << endl;
+    return 0;
+}
