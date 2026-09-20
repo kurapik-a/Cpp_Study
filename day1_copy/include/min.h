ddea3e4b21bee64910638864ec738a5553766e91
杜宏泰 <2535692722@qq.com>
2026-09-20 16:15:04 +0800

    标准工程

---
+++ day1_copy/include/min.h
@@ -0,0 +1,6 @@
+#ifndef MIN_H
+#define MIN_H
+
+int min(int a, int b);
+
+#endif // MIN_H
---
+++ day1_copy/src/test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "min.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "hello world" << endl;
+    cout << min(100, 200) << endl;
+    return 0;
+}
